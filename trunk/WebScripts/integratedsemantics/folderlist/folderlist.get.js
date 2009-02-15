b31ba0efb5aedc39b05ec2ed994291a6092d664c
stevereiner@integratedsemantics.com <stevereiner@integratedsemantics.com@d70f39b3-6552-0410-a7a6-41458c692471>
2009-02-15 02:15:18 +0000

    search results, folderlist webscripts: added serverside paging, returning mimetype

--- trunk/WebScripts/integratedsemantics/folderlist/folderlist.get.js
+++ trunk/WebScripts/integratedsemantics/folderlist/folderlist.get.js
@@ -4,7 +4,6 @@ if ((args.nodeid) && (args.nodeid != ""))
    model.node = search.findNode("workspace://SpacesStore/" + args.nodeid);
 }
 
-
 if (model.node == null)
 {
    if ((args.path) && (args.path != ""))
@@ -19,3 +18,27 @@ if (model.node == null)
       }   
    }
 }
+
+var children = model.node.children;
+model.totalSize = children.length;
+
+if ((args.pagesize != undefined) && (args.pagenum != undefined))
+{
+   var pageSize = parseInt(args.pagesize);
+   var pageNum = parseInt(args.pagenum);
+
+   var begin = pageNum * pageSize;
+   var end = begin + pageSize;
+   
+   model.children = children.slice(begin, end);
+   
+   model.pageSize = pageSize;
+   model.pageNum = pageNum;
+}
+else
+{
+   model.children = children;
+   
+   model.pageSize = 0;
+   model.pageNum = 0;   
+}   
